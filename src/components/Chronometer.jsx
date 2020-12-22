8cd529231ed0184af9848d60ecf9bd31f7a05a4d
gabriel-codes <gabrielss@outlook.com>
2020-12-22 02:19:56 -0300

    Create Timer Function

--- src/components/Chronometer.jsx
+++ src/components/Chronometer.jsx
@@ -6,6 +6,10 @@ import { FaPlayCircle } from 'react-icons/fa';
 import { FaPauseCircle } from 'react-icons/fa';
 import { FaStopCircle } from 'react-icons/fa';
 import { FaPlusCircle } from 'react-icons/fa';
+import { FaClock } from 'react-icons/fa';
+import { FaStopwatch } from 'react-icons/fa';
+import { FaChevronUp } from 'react-icons/fa';
+import { FaChevronDown } from 'react-icons/fa';
 
 export default class Chronometer extends Component {
   constructor(props) {
@@ -15,6 +19,7 @@ export default class Chronometer extends Component {
     this.state = {
       counter: 0,
       stoped: true,
+      timer: false,
     };
     this.options = {
       hour: 0,
@@ -27,15 +32,30 @@ export default class Chronometer extends Component {
     this.play = this.play.bind(this);
     this.stop = this.stop.bind(this);
     this.partial = this.partial.bind(this);
+    this.toggleTimer = this.toggleTimer.bind(this);
+    this.addHour = this.addHour.bind(this);
+    this.removeHour = this.removeHour.bind(this);
+    this.addMinutes = this.addMinutes.bind(this);
+    this.removeMinutes = this.removeMinutes.bind(this);
+    this.addSeconds = this.addSeconds.bind(this);
+    this.removeSeconds = this.removeSeconds.bind(this);
+    this.chronoUpdate = this.chronoUpdate.bind(this);
     Dayjs.extend(objectSupport);
     this.chrono = Dayjs(this.options).add({ ms: this.state.counter });
   }
 
   tick() {
-    this.setState((state) => ({
-      counter: state.counter + 10,
-      stoped: false,
-    }));
+    if (this.state.timer) {
+      this.setState((state) => ({
+        counter: state.counter - 10,
+        stoped: false,
+      }));
+    } else {
+      this.setState((state) => ({
+        counter: state.counter + 10,
+        stoped: false,
+      }));
+    }
   }
 
   pause() {
@@ -49,9 +69,6 @@ export default class Chronometer extends Component {
 
   play() {
     this.interval = setInterval(() => this.tick(), 10);
-    this.setState({
-      stoped: false,
-    });
   }
 
   stop() {
@@ -65,19 +82,55 @@ export default class Chronometer extends Component {
     this.interval = null;
   }
 
+  toggleTimer() {
+    this.setState((state) => ({
+      timer: !state.timer,
+    }));
+    this.stop();
+  }
+
   partial() {
-    this.setPartial(this.timer);
+    this.currentPartial = `${this.hour}:${this.minute}:${this.second}:${this.milli}`;
+    this.setPartial(this.currentPartial);
   }
 
-  componentDidUpdate() {
+  addHour() {
+    this.setState((state) => ({
+      counter: state.counter + 3600000,
+    }));
+  }
+
+  removeHour() {
+    this.setState((state) =>
+      state.counter > 0 ? { counter: state.counter - 3600000 } : { counter: 0 },
+    );
+  }
+
+  addMinutes() {
+    this.setState((state) => ({
+      counter: state.counter + 60000,
+    }));
+  }
+
+  removeMinutes() {
+    this.setState((state) =>
+      state.counter > 0 ? { counter: state.counter - 60000 } : { counter: 0 },
+    );
+  }
+  addSeconds() {
+    this.setState((state) => ({
+      counter: state.counter + 1000,
+    }));
+  }
+
+  removeSeconds() {
+    this.setState((state) =>
+      state.counter > 0 ? { counter: state.counter - 1000 } : { counter: 0 },
+    );
+  }
+
+  chronoUpdate() {
     this.chrono = Dayjs(this.options).add({ ms: this.state.counter });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
     this.hour =
       this.chrono.hour() < 10 ? '0' + this.chrono.hour() : this.chrono.hour();
     this.minute =
@@ -94,24 +147,65 @@ export default class Chronometer extends Component {
         : this.chrono.millisecond() < 100
         ? '0' + this.chrono.millisecond()
         : this.chrono.millisecond();
+  }
 
-    this.timer = `${this.hour}:${this.minute}:${this.second}:${this.milli}`;
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
+  componentDidUpdate() {
+    if (this.state.counter <= 0 && !this.state.stoped) {
+      this.stop();
+    }
+  }
+
+  render() {
+    this.chronoUpdate();
     return (
       <>
         <div className="chrono">
           <div className="chrono__timer">
             <div className="chrono__counter">
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.addHour}>
+                  <FaChevronUp size={25} color="#fff" />
+                </button>
+              )}
               {this.hour}
               <p>hour</p>
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.removeHour}>
+                  <FaChevronDown size={25} color="#fff" />
+                </button>
+              )}
             </div>
             <div className="chrono__counter">
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.addMinutes}>
+                  <FaChevronUp size={25} color="#fff" />
+                </button>
+              )}
               {this.minute}
               <p>minutes</p>
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.removeMinutes}>
+                  <FaChevronDown size={25} color="#fff" />
+                </button>
+              )}
             </div>
             <div className="chrono__counter">
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.addSeconds}>
+                  <FaChevronUp size={25} color="#fff" />
+                </button>
+              )}
               {this.second}
               <p>seconds</p>
+              {this.state.timer && (
+                <button className="chrono__button" onClick={this.removeSeconds}>
+                  <FaChevronDown size={25} color="#fff" />
+                </button>
+              )}
             </div>
             <div className="chrono__milli">
               {this.milli}
@@ -120,6 +214,13 @@ export default class Chronometer extends Component {
           </div>
         </div>
         <div className="chrono__buttons">
+          <button className="chrono__button" onClick={this.toggleTimer}>
+            {this.state.timer ? (
+              <FaStopwatch size={25} />
+            ) : (
+              <FaClock size={25} />
+            )}
+          </button>
           <button
             className="chrono__button"
             disabled={!this.state.stoped}
